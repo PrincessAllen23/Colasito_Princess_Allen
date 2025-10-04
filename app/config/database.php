230401726ce0e62fc10817928f5f9180e2813eea
Colasito_PrincessAllen <princessallenc@gmail.com>
2025-10-05 01:12:48 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12800432',
-    'password'	=> 'zXs38yrwFg' ,
-    'database'	=> 'sql12800432',
+    'username'	=> 'sql12800444',
+    'password'	=> 'sdYk1Xzb4R' ,
+    'database'	=> 'sql12800444',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
